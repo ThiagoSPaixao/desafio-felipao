8a321230a3856f2813badc4cf5be43612064c681
ThiaguinhoxD <thiaguinhoescolhido26@gmail.com>
2023-12-16 21:55:18 -0300

    Desafio Concluído

---
+++ desafio-felipao.js
@@ -0,0 +1,37 @@
+//Desafio Classificador de Nível de Herói
+//O que deve ser Utilizado:
+// - Variáveis - Operadores - Laços de Repetição - Estruturas de Decisões
+//Lá vai...
+
+
+let nomeDoHeroi = "ThiaguinhoxD";
+let classeDoHeroi = "Guerreiro";
+let xpDoHeroi = 50000;
+
+if (xpDoHeroi <= 1000) {
+    console.log("O Herói " + nomeDoHeroi + " da Classe " + classeDoHeroi + " Está no nível Ferro");
+} else if (xpDoHeroi <= 2000) {
+    console.log("O Herói " + nomeDoHeroi + " da Classe " + classeDoHeroi + " Está no nível Bronze");
+} else if (xpDoHeroi <= 5000) {
+    console.log("O Herói " + nomeDoHeroi + " da Classe " + classeDoHeroi + " Está no nível Prata");
+} else if (xpDoHeroi <= 7000) {
+    console.log("O Herói " + nomeDoHeroi + " da Classe " + classeDoHeroi + " Está no nível Ouro");
+} else if (xpDoHeroi <= 8000) {
+    console.log("O Herói " + nomeDoHeroi + " da Classe " + classeDoHeroi + " Está no nível Platina");
+} else if (xpDoHeroi <= 9000) {
+    console.log("O Herói " + nomeDoHeroi + " da Classe " + classeDoHeroi + " Está no nível Ascendente");
+} else if (xpDoHeroi <= 10000) {
+    console.log("O Herói " + nomeDoHeroi + " da Classe " + classeDoHeroi + " Está no nível Imortal");
+} else {
+    console.log("O Herói " + nomeDoHeroi + " da Classe " + classeDoHeroi + " Está no nível Radiante");
+}
+
+    
+
+    
+
+
+
+
+
+
--- index.js
+++
@@ -1,3 +0,0 @@
-console.log("olá futuro programador de sucesso!")
-console.log("vamos estudar, porque a gente nasceu inteligente, não rico! kkk")
-console.log("DEUS é Fiel, Jesus te ama!")
